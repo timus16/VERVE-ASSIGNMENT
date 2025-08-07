6d6c0cc1c035358bbde8fab5e18162bd3bb368cd
Sumit Gupta <timus1666@gmail.com>
2025-08-07 22:32:45 +0530

    Basic Theme Setup

---
+++ wp-content/themes/marshalle/index.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * 
+ * Main Tempalte File
+ * 
+ * @package verve-assignment
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verve-assignment</title>
+</head>
+<body>
+    
+</body>
+</html>
